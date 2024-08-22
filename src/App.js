e35d720fd1ab74ee35ee281fb74141fed93f3142
TolgaBeyzatov <115338509+TolgaBeyzatov@users.noreply.github.com>
2024-08-22 13:22:36 +0000

    Changed title

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          This is a test project for CI/CD
         </a>
       </header>
       <p>
